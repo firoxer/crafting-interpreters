6acc29659d9d0ff6a8d73dd933e399e7fc45fd62
Oliver Vartiainen <oliver@iki.fi>
2023-02-22 22:55:32 +0200

    Chapter 10

---
+++ src/fi/rakkine/lox/LoxCallable.java
@@ -0,0 +1,9 @@
+package fi.rakkine.lox;
+
+import java.util.List;
+
+interface LoxCallable {
+    int arity();
+    
+    Object call(Interpreter interpreter, List<Object> arguments);
+}
--- src/fi/rakkine/lox/Parser.java
+++ src/fi/rakkine/lox/Parser.java
@@ -31,6 +31,8 @@ class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(FUN))
+                return function("function");
             if (match(VAR))
                 return varDeclaration();
 
@@ -48,6 +50,8 @@ class Parser {
             return ifStatement();
         if (match(PRINT))
             return printStatement();
+        if (match(RETURN))
+            return returnStatement();
         if (match(WHILE))
             return whileStatement();
         if (match(LEFT_BRACE))
@@ -112,6 +116,16 @@ class Parser {
         return new Stmt.Print(value);
     }
 
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON))
+            value = expression();
+
+        consume(SEMICOLON, "Expected ';' after return value");
+        return new Stmt.Return(keyword, value);
+    }
+
     private Stmt varDeclaration() {
         Token name = consume(IDENTIFIER, "Expected variable name");
 
@@ -136,6 +150,26 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expected ';' after value");
         return new Stmt.Expression(expr);
+    }
+
+    private Stmt.Function function(String kind) {
+        Token name = consume(IDENTIFIER, "Expected " + kind + " name");
+        consume(LEFT_PAREN, "Expected '(' after " + kind + " name");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Can't have more than 255 parameters");
+                }
+
+                parameters.add(consume(IDENTIFIER, "Expected parameter name"));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Expected ')' after parameters");
+
+        consume(LEFT_BRACE, "Expected '{' before " + kind + " body");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     private List<Stmt> block() {
@@ -245,7 +279,33 @@ class Parser {
             return new Expr.Unary(operator, right);
         }
 
-        return primary();
+        return call();
+    }  
+
+    private Expr finishCall(Expr callee) {
+        List<Expr> arguments = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                arguments.add(expression());
+            } while (match(COMMA));
+        }
+
+        Token paren = consume(RIGHT_PAREN, "Expected ')' after arguments");
+
+        return new Expr.Call(callee, paren, arguments);
+    }
+
+    private Expr call() {
+        Expr expr = primary();
+
+        while (true) {
+            if (match(LEFT_PAREN))
+                expr = finishCall(expr);
+            else
+                break;
+        }
+
+        return expr;
     }
 
     private Expr primary() {
